d92aa6bd5f6862d8961ead492d984c1fda516280
michaelPE16 <rafael112216@gmail.com>
2025-03-28 03:49:31 -0400

    adding_env

--- app_railway/settings.py
+++ app_railway/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'app_railway.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
